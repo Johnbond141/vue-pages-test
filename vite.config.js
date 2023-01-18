cc107bbd0ed80f1e084caa566b457c2a099d580d
John <johnbond@live.nl>
2023-01-18 13:40:42 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('/vue-pages-test/src', import.meta.url))
     }
   }
 })
